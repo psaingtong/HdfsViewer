5fa73a2e88d3188a20df11db34ab44764c71994f
psaingtong <psaingtong@gmail.com>
2018-10-17 07:15:26 +0700

    add new method

--- src/main/java/com/puttel/app/MainPanel.java
+++ src/main/java/com/puttel/app/MainPanel.java
@@ -1,11 +1,16 @@
 package com.puttel.app;
 
+import com.alee.extended.layout.ToolbarLayout;
+import com.alee.extended.statusbar.WebMemoryBar;
+import com.alee.extended.statusbar.WebStatusBar;
+import com.alee.laf.button.WebButton;
 import com.alee.laf.desktoppane.WebDesktopPane;
 import com.alee.laf.rootpane.WebFrame;
 import com.puttel.app.utils.I18n;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -26,14 +31,28 @@ public class MainPanel extends WebFrame {
         });
         setTitle("Hdfs Viewer");
         getContentPane().add(desktopPane, BorderLayout.CENTER);
+        getContentPane().add(createStatusBar (), BorderLayout.PAGE_END);
         setLocationRelativeTo(null);
         pack();
         setVisible(true);
     }
-    public boolean confirmBeforeExit(){
+    private boolean confirmBeforeExit(){
         if (JOptionPane.showConfirmDialog(this, I18n.lang("desktop.confirmbeforeexitdialog.title"), I18n.lang("desktop.confirmbeforeexitdialog.text"), JOptionPane.YES_NO_OPTION) == 0){
             return true;
         }
         return false;
+    }
+    private WebStatusBar createStatusBar() {
+        WebStatusBar result = new WebStatusBar();
+        result.add(this.createButton("Connect"));
+
+
+        return result;
+    }
+    private WebButton createButton(String text) {
+        WebButton button = new WebButton(text);
+        button.setFontSize(12);
+        button.setMinimumWidth(80);
+        return button;
     }
 }
